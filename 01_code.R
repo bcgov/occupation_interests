1107c5438a97ba46d824a31d2d310ba55c090dd5
Rich-P-Martin <richard.martin@gov.bc.ca>
2024-03-25 13:19:35 -0700

    smushing the addional 5 occupations to original file

--- 01_code.R
+++ 01_code.R
@@ -3,26 +3,12 @@ library(here)
 library(readxl)
 library(janitor)
 
+original_interests <- read_excel(here("data","Occupational interest by NOC2021 occupation.xlsx"))
 
-desired_nocs <- read_excel(
-  here("data", "Occupational Characteristics w skills interests wages.xlsx"),
-  skip = 0,
-  na = "x"
-  )|>
-  select(NOC)
+five_more <- read_csv(here("data","five_more.csv"))|>
+  mutate(`NOC 2021`=str_pad(`NOC 2021`, width=5, side = "left", pad = "0"))|>
+  pivot_longer(cols = 3:5, names_to = "Options", values_to = "Occupational Interest")
 
-interests <- read_excel(here("data","Interests.xlsx"))
-mapping <- read_excel(here('data',"onet2019_soc2018_noc2016_noc2021_crosswalk.xlsx"))|>
-  mutate(noc2021=str_pad(noc2021, width=5, "left","0"),
-         noc2021=paste0("#",noc2021)
-  )
-
-full_join(interests, mapping,
-                    by=join_by("O*NET-SOC Code"=="onetsoc2019"),
-                    relationship = "many-to-many")|>
-  filter(`Scale Name`=="Occupational Interests")|>
-  group_by(noc2021, `Element Name`)|>
-  summarise(`Data Value`=mean(`Data Value`, na.rm = TRUE))|>
-  group_by(noc2021, .add = FALSE)|>
-  slice_max(`Data Value`, n=3, with_ties = FALSE)|>
-  write_csv(here("out","top3_interests_by_noc.csv"))
+full_join(original_interests, five_more)|>
+  arrange(`NOC 2021`)|>
+  openxlsx::write.xlsx(here("out", "Occupational interest by NOC2021 occupation_plus_5.xlsx"))
